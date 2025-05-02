6e6ae8dc6c038b1c2406f4eb4e0f496788b41eef
Jimmy Feng Feng <jimmy.feng04@gmail.com>
2025-05-01 19:38:33 -0600

    [ADD] Modelos para Estudiante y Profesor

--- CEDigital_api/Models/Mongo/Estudiante.cs
+++ CEDigital_api/Models/Mongo/Estudiante.cs
@@ -1,6 +1,22 @@
-﻿namespace CEDigital_api.Models.Mongo
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CEDigital_api.Models.Mongo
 {
     public class Estudiante
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string carnet { get; set; }
+
+        public string cedula { get; set; }
+        public string nombre { get; set; }
+        public string apellido1 { get; set; }
+        public string apellido2 { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+        public string password { get; set; }
+
+
     }
 }
--- CEDigital_api/Models/Mongo/Profesor.cs
+++ CEDigital_api/Models/Mongo/Profesor.cs
@@ -1,6 +1,18 @@
-﻿namespace CEDigital_api.Models.Mongo
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CEDigital_api.Models.Mongo
 {
     public class Profesor
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string cedula { get; set; }
+
+        public string nombre { get; set; }
+        public string apellido1 { get; set; }
+        public string apellido2 { get; set; }
+        public string correo { get; set; }
+        public string password { get; set; }
     }
 }
