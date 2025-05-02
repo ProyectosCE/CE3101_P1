d244fafeab3dbac6f8f419c91140f043b91af526
Jimmy Feng Feng <jimmy.feng04@gmail.com>
2025-05-01 19:44:40 -0600

    [ADD] MongoDB Settings

---
+++ CEDigital_api/Data/Mongo/MongoDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace CEDigital_api.Data.Mongo
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionURI { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string CollectionName { get; set; } = null!;
+    }
+}
