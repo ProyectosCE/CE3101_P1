610d980ac75071c89f84be57208eae49c346f0d2
Jimmy Feng Feng <jimmy.feng04@gmail.com>
2025-05-02 19:59:57 -0600

    [UPDATE] SQL connection con api

--- CEDigital_api/Program.cs
+++ CEDigital_api/Program.cs
@@ -1,4 +1,6 @@
 using CEDigital_api.Data.Mongo;
+using CEDigital_api.Data.Sql;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,10 @@ builder.Services.Configure<MongoDbSettings>(
 
 builder.Services.AddSingleton<ProfesorService>();
 builder.Services.AddSingleton<EstudianteService>();
+
+// SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
