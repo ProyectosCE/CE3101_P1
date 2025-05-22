be3006fbb4a496e5117863b58acc92232cbb2fc5
AdrMuAl <131110706+AdrMuAl@users.noreply.github.com>
2025-05-21 19:17:01 -0600

    Update AuthService.cs
    Ahora tmb devuelve id

--- CEDigital_api/Data/Mongo/AuthService.cs
+++ CEDigital_api/Data/Mongo/AuthService.cs
@@ -19,21 +19,21 @@ namespace CEDigital_api.Data.Mongo
             _profesores = database.GetCollection<Profesor>("profesores");
         }
 
-        public async Task<(bool success, string role, string message)> LoginAsync(string correo, string password)
+        public async Task<(bool success, string role, string message, string id)> LoginAsync(string correo, string password)
         {
             var profesor = await _profesores.Find(p => p.correo == correo).FirstOrDefaultAsync();
             if (profesor != null && BCrypt.Net.BCrypt.Verify(password, profesor.password))
             {
-                return (true, profesor.IsAdmin ? "admin" : "profesor", "Inicio de sesión exitoso");
+                return (true, profesor.IsAdmin ? "admin" : "profesor", "Inicio de sesión exitoso", profesor.Id);
             }
 
             var estudiante = await _estudiantes.Find(e => e.correo == correo).FirstOrDefaultAsync();
             if (estudiante != null && BCrypt.Net.BCrypt.Verify(password, estudiante.password))
             {
-                return (true, "estudiante", "Inicio de sesión exitoso");
+                return (true, "estudiante", "Inicio de sesión exitoso", estudiante.Id);
             }
 
-            return (false, "", "Credenciales inválidas");
+            return (false, "", "Credenciales inválidas", "");
         }
     }
 }
